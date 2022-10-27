831c3bd173270b8350820c1d231a44a9e66d86e3
joojn <85359001+joojn1122@users.noreply.github.com>
2022-10-27 22:55:43 +0200

    Add files via upload

---
+++ Hypixel-GuessTheBuild-Helper/src/main/java/me/joojn/guesser/guess/GuessTheBuild.java
@@ -0,0 +1,147 @@
+package me.joojn.guesser.guess;
+
+import me.joojn.guesser.GuesserMod;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GuessTheBuild {
+
+    public static final String PREFIX = "§8[§6Guess The Builder Helper§8]: §f";
+
+    // regex
+    private static final Pattern originalSizePattern = Pattern.compile("(?<=\\[).*(?=])");
+
+    public static boolean inGame = false;
+    private static boolean showAllWords = true;
+
+    private static final List<String> filteredWords = new ArrayList<>();
+    public static String[] words = getWords();
+
+
+    static{
+        System.out.printf("Found words %s\n", Arrays.toString(words));
+    }
+
+    private static String[] getWords()
+    {
+        File file = new File("guesser-words.csv");
+
+        if(file.exists())
+        {
+            try
+            {
+                GuesserMod.LOGGER.info("Trying to load words from '%s'".formatted(file.getName()));
+
+                return FileUtils.readFileToString(file, StandardCharsets.UTF_8).split(";");
+            }
+            catch (IOException e)
+            {
+                GuesserMod.LOGGER.error("Something went wrong while trying to load data from '%s'".formatted(file.getName()));
+
+                e.printStackTrace();
+            }
+        }
+
+        try
+        {
+            URL originalSite = new URL("https://gtb.cyberfla.me/wordlist/wordlist.js");
+            GuesserMod.LOGGER.info("Trying to load words from %s".formatted(originalSite.getPath()));
+
+            String data = IOUtils.toString(originalSite, StandardCharsets.UTF_8)
+                    .replace("\n", "")
+                    .replace("\"", "")
+                    .replace("\t", "")
+                    .replace("\r", "");
+
+            Matcher match = originalSizePattern.matcher(data);
+            if(!match.find()) throw new IOException("Something went wrong while searching for array..");
+
+            return match.group(0).split(",");
+        }
+        catch (IOException e)
+        {
+            GuesserMod.LOGGER.error("Could not load words from original site..");
+
+            e.printStackTrace();
+        }
+
+        try
+        {
+            URL url = new URL("https://raw.githubusercontent.com/joojn1122/Fabric-mods/main/Hypixel-GuessTheBuild-Helper/words.csv");
+
+            GuesserMod.LOGGER.info("Trying to load words from %s".formatted(url.getPath()));
+
+            return IOUtils.toString(url, StandardCharsets.UTF_8).split(";");
+        }
+        catch (IOException e)
+        {
+            GuesserMod.LOGGER.error("Could not load words from github..");
+
+            e.printStackTrace();
+        }
+
+        return new String[0];
+    }
+
+    private static String theme_ = "";
+
+    public static void setTheme(String theme)
+    {
+        if(Objects.equals(theme, theme_)) return;
+
+        theme_ = theme;
+
+        if(theme != null)
+        {
+            filteredWords.clear();
+            showAllWords = false;
+
+            GuesserMod.addChatMessage("§aSetting current search to '§e%s§a'".formatted(theme));
+
+            Pattern themePattern = Pattern.compile(
+                    "(?i)" + theme.replace("_", "[^ ]")
+            );
+
+            for(String s : words)
+            {
+                if(
+                   themePattern.matcher(s).find())
+                {
+                    filteredWords.add(s);
+                }
+            }
+        }
+        else
+        {
+            showAllWords = true;
+        }
+    }
+
+    public static String[] getFilteredWords()
+    {
+        return showAllWords ? words : filteredWords.toArray(new String[0]);
+    }
+
+    public static int updateWords(CommandContext<FabricClientCommandSource> context)
+    {
+        context.getSource().sendFeedback(
+                Text.literal(PREFIX + "§aUpdating words..")
+        );
+
+        words = getWords();
+
+        return 1;
+    }
+}
---
+++ Hypixel-GuessTheBuild-Helper/src/main/java/me/joojn/guesser/guess/SuggestCommander.java
@@ -0,0 +1,89 @@
+package me.joojn.guesser.guess;
+
+import me.joojn.guesser.GuesserMod;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.command.CommandRegistryAccess;
+
+import java.util.concurrent.CompletableFuture;
+
+public class SuggestCommander implements SuggestionProvider<FabricClientCommandSource> {
+
+    public static void register(
+            CommandDispatcher<FabricClientCommandSource> dispatcher,
+            CommandRegistryAccess commandRegistryAccess
+    )
+    {
+        dispatcher.register(
+                ClientCommandManager.literal(
+                        "guesser"
+                )
+                        .then(
+                                ClientCommandManager.literal("update")
+                                        .executes(GuessTheBuild::updateWords)
+                        )
+        );
+
+        LiteralCommandNode<FabricClientCommandSource> node = dispatcher.register(
+                ClientCommandManager.literal(
+                        "s"
+                ).then(
+                        ClientCommandManager.argument("word", StringArgumentType.string())
+                                .suggests(new SuggestCommander())
+                                .executes(SuggestCommander::execute)
+                )
+        );
+
+        // alias of s
+        dispatcher.register(
+                ClientCommandManager.literal(
+                        "s_"
+                ).redirect(node)
+        );
+    }
+
+    private static int execute(CommandContext<FabricClientCommandSource> context)
+    {
+        String input = context.getArgument("word", String.class);
+
+        GuesserMod.sendChatMessage(input);
+
+        return 0;
+    }
+
+
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(
+            CommandContext<FabricClientCommandSource> context,
+            SuggestionsBuilder builder
+    )
+    {
+        String input = context.getInput().substring(
+                context.getInput().indexOf(" ") + 1
+                ).replace("\"", "").toLowerCase();
+
+        for(String s : GuessTheBuild.getFilteredWords())
+        {
+            if(!(
+                    s.toLowerCase().startsWith(input)
+                    || (s.contains(" ") && s.split(" ")[1].startsWith(input))
+            )) continue;
+
+            if(s.contains(" "))
+            {
+                builder.suggest('"' + s + '"');
+            }
+            else builder.suggest(s);
+        }
+
+        return builder.buildFuture();
+    }
+
+}
